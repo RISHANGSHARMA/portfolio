7fdf184b624a1dcd4fa72a33dcbad245afff0500
rishangswapnb <rishangswapnb@gmail.com>
2025-09-09 19:47:26 -0400

    GPA

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -40,7 +40,7 @@ const Navbar: React.FC = () => {
           </Link>
           <ul className="navbar-links">
             <li><Link to="/browse">Home</Link></li>
-            <li><Link to="/work-experience">Professional</Link></li>
+            <li><Link to="/work-experience">Watch History</Link></li>
             <li><Link to="/skills">Skills</Link></li>
             <li><Link to="/projects">Projects</Link></li>
             <li><Link to="/contact-me">Hire Me</Link></li>
